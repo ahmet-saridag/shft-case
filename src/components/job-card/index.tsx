504f8cdc86fd189d28689bd269df56a1ecfc280b
AHMET SARIDAG <ahmet.saridagj@gmail.com>
2024-05-17 01:43:42 +0300

    popup shadow

--- src/components/job-card/index.tsx
+++ src/components/job-card/index.tsx
@@ -129,10 +129,10 @@ export default function JobCard({jobsData}: any) {
                       {
                 isModalOpen && (
                   <div
-                  className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
+                  className=" overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full md:inset-0 max-h-full"
                 >
                   <div className="relative p-4 w-full max-w-2xl max-h-full">
-                    <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                    <div className="shadow-2xl relative bg-white rounded-lg dark:bg-gray-700">
                       <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                         <h3 className="text-4xl w-full text-center  font-semibold text-gray-900 dark:text-white">
                           Apply Job
